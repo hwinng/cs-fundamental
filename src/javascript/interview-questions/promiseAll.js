e2a224ac1d0dcc72b74625173c8a2ca53eda44b0
Phillip Nguyen <indieretro20s@gmail.com>
2022-07-27 22:34:39 +0700

    implement promise all

--- src/javascript/interview-questions/promiseAll.js
+++ src/javascript/interview-questions/promiseAll.js
@@ -1,26 +1,40 @@
-const promiseAll = (promises) => {
-    if (!Array.isArray(promises)) {
-        throw new Error('Expect an array of promises');
-    }
 
+function promiseAll(promises) {
     return new Promise((resolve, reject) => {
-        let results = [];
+        const results = [];
+
+        if (!promises.length) {
+            resolve(results);
+            return;
+        }
+
+        let pending = promises.length;
         promises.forEach((promise, idx) => {
-            promise
-                .then(res => {
-                    results.push(res);
+            Promise.resolve(promise)
+                .then((value) => {
+                    results[idx] = value;
+                    pending--;
+                    if (pending === 0) {
+                        resolve(results);
+                    }
                 })
-                .catch(err => {
-                    console.log('rejected')
-                    reject(`Promise at index ${idx} is rejected`);
-                })
-        })
-        resolve(results);
-    })
-}
+                .catch(err => reject(err))
+        });
+    });
+};
 
-const promises = [Promise.resolve(1), Promise.reject(2)];
+const case_1 = [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]; // [1, 2, 3]
+const case_1_1 = ['DONE!', Promise.resolve(2), Promise.resolve(3)]; // ['DONE!', 2, 3]
+const case_1_2 = [Promise.resolve(1), Promise.resolve(2), 3]; // [1, 2, 3]
 
-const promise_test = promiseAll(promises);
+const case_2 = [Promise.reject('REJECTED_2'), Promise.resolve('Phillip'), Promise.resolve('Nguyen')]; // REJECT_2
+const case_2_1 = [Promise.resolve('a_2_1'), Promise.resolve('b'), Promise.reject('REJECT_3'), Promise.resolve('c')]; // REJECT_3
+const case_2_2 = [Promise.resolve('a_2_2'), Promise.resolve('Phillip'), Promise.reject('CASE')]; // CASE
 
-promise_test.then(console.log)
+promiseAll(case_1).then(console.log).catch(console.log);
+promiseAll(case_1_1).then(console.log).catch(console.log);
+promiseAll(case_1_2).then(console.log).catch(console.log);
+promiseAll(case_2).then(console.log).catch(console.log);
+promiseAll(case_2_1).then(console.log).catch(console.log);
+promiseAll(case_2_2).then(console.log).catch(console.log);
+
